4936a6abe8120684d58c9966c428663a2788907f
justin-ngatho <93423017+justin-ngatho@users.noreply.github.com>
2023-11-19 14:18:15 +0300

    Update nav.js
    logo changed

--- src/components/nav.js
+++ src/components/nav.js
@@ -10,7 +10,7 @@ export default function Navbar () {
     return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="#"><span className="className text-warning">Cons</span>Traction</a>
+        <a className="navbar-brand" href="#"><span className="className text-warning">Alpha</span>Sets</a>
         <button className="navbar-toggler" type="button" onClick={toggleButton} data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" ></span>
         </button>
